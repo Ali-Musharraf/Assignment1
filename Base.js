7fecdd5fa8b232e6808e2177140ef740f68f6cf2
Ali-Musharraf <musharrafali17696@gmail.com>
2023-01-07 23:17:55 +0530

    Adding base files

---
+++ Base.js
@@ -0,0 +1,4 @@
+const header = document.createElement("h2");
+header.innerText = "This is from JavaScript";
+const root = document.getElementById("root");
+document.getElementById("root").appendChild(header);
